bbcf064c8c8ce432e1d3af925aa77037dd274b56
Albert Liang <albert.liang@gmail.com>
2018-02-13 14:13:15 -0600

    created csv output files for everything

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 
-from __future__ import division
 # python3 division can handle int/int=float, whereas python2 returns an int
+from __future__ import division
 
-
+# on my laptop, the path to Python is incomplete and it cant find where pip packages are installed
 import sys
 sys.path.append('/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages')
-
 
 import csv
 import tinydb
@@ -21,7 +20,6 @@ def main():
   # print some newlines to look prettier
   print '\n'*2
   print "### Script output and debug information ###"
-
 
   # set up categories
   catdb = import_csv('./categories.csv','./categories.json','catdb',True)
@@ -42,33 +40,47 @@ def main():
   # remove positive-value entries
   mtdb.remove(tinydb.Query().amount > 0)
 
-  # scan for categories and report errors
+  # assign categories to each item and report errors
   for cat in catdb:
     # detect and record multiple-tagging
     for f in mtdb.search( (tinydb.Query().description.search(cat['item'])) & (tinydb.Query().tag.exists()) ):
       print "Double tag:     %-8s %-15s %-15s %s" % (f['amount'],f['tag'],cat['tag'],f['description'])
     mtdb.update(tinydb.operations.set('tag',cat['tag']),tinydb.Query().description.search(cat['item']))
-
   for f in mtdb.search(~ tinydb.Query().tag.exists()):
     print "Untagged entry: %-40s %s" % (f['amount'],f['description'])
 
 
-  # build list of unique tags
+  print_output(mtdb)
+# end main
+
+
+def print_output(mtdb):
+  # build uniqe list of tags used in db
   uniqtags = set(f['tag'] for f in mtdb.search(tinydb.Query().tag.exists()))
 
+  # build list of dates
+  uniqdays=[f['transdate'] for f in mtdb.all()]
+  uniqdays=set(uniqdays)  # get unique values
+  uniqdays=list(uniqdays)  # convert it back into list, because order matters
+  uniqdays=sorted(uniqdays,key=lambda x:datetime.datetime.strptime(x,'%m/%d/%Y'))
 
   #### output fun statistics ####
   print '\n'*2
 
   print "### overall statistics ###"
+  resrow=[]
   sumtot=0
   for f in mtdb.all():
     sumtot+=f['amount']
-  print "Total amount spent:      %0.2f" % (sumtot)
-  print "Number of items:         %d"    % (len(mtdb))
-  print "Average amount per item: %0.2f" % (sumtot/len(mtdb))
+  resrow.append(['Total amount spent',sumtot])
+  resrow.append(['Number of items',len(mtdb)])
+  resrow.append(['Average amount per item',"%0.2f" % (sumtot/len(mtdb))])  # this method rounds to nearest cent value
+  resrow.append(['Start date',uniqdays[0]])
+  resrow.append(['End date',uniqdays[-1]])
+  for f in resrow:
+    print "%-30s: %10s" % (f[0],f[1])
+  save_csv(resrow,"overview.csv")
 
-  # todo: sort by total amount
   # total sum per category
   print ""
   print "### spending by category ###"
@@ -82,23 +94,27 @@ def main():
   # sort by sumtot
   for f in sorted(resrow, key=operator.itemgetter(1)):
     print "%-15s: %10.2f (number of items: %3d, avg spend per item: %10.2f)" % (f[0],f[1],f[2],f[3])
+  save_csv(resrow,"spending_per_category.csv")
 
   # money spent per day
   plusfac=-50
   print ""
   print "### spending by day ###"
-  day=[f['transdate'] for f in mtdb.all()]
-  day=set(day)  # get unique values
-  day=list(day)  # convert it back into list, because order matters
-  day=sorted(day,key=lambda x:datetime.datetime.strptime(x,'%m/%d/%Y'))
-  for tag in day:
+  resrow=[]
+  for tag in uniqdays:
     sumtot = 0
     for f in mtdb.search(tinydb.Query().transdate == tag):
       sumtot += f['amount']
+    resrow.append([tag,sumtot])
 
-    sys.stdout.write("%-10s: %8.2f   " % (tag,sumtot))
+    #sys.stdout.write("%-10s: %8.2f   " % (tag,sumtot))
+    #sys.stdout.flush()
+    #print "+"*(int(sumtot/plusfac))
+  for f in resrow:
+    sys.stdout.write("%-10s: %8.2f   " % (f[0],f[1]))
     sys.stdout.flush()
-    print "+"*(int(sumtot/plusfac))
+    print "+"*(int(f[1]/plusfac))
+  save_csv(resrow,"spending_per_day.csv")
 
   # highest spend per category
   print ""
@@ -119,6 +135,7 @@ def main():
   for f in sorted(resrow, key=operator.itemgetter(2)):
     print "%-15s %-30s %8.2f" % (f[0],f[1],f[2])
   print "%-46s %8.2f" % ("--- total ---",sumtot)
+  save_csv(resrow,"highest_spend_per_category.csv")
 
   # list items over a certain threshold
   threshold=-100
@@ -135,7 +152,8 @@ def main():
   for f in sorted(resrow, key=operator.itemgetter(2)):
     print "%-15s %-30s %8.2f" % (f[0],f[1],f[2])
   print "%-46s %8.2f" % ("--- total ---",sumtot)
-# end main
+  save_csv(resrow,"items_over_spend_threshold.csv")
+# end print_output
 
 
 
@@ -152,7 +170,7 @@ def import_csv(file,db_name,db_handle,purge = False):
   headers=[k.lower() for k in headers]
   line_items=[[k.lower() for k in i] for i in line_items]
 
-  # remove spaces
+  # remove spaces in header
   headers=[k.replace(' ','') for k in headers]
 
   db_handle = tinydb.TinyDB(db_name)
@@ -166,6 +184,13 @@ def import_csv(file,db_name,db_handle,purge = False):
 
   return db_handle
 # end import_csv
+
+
+def save_csv(input_list,outfile):
+  with open(outfile,'wb') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerows(input_list)
+# end save_csv
 
 
 if __name__ == "__main__":
